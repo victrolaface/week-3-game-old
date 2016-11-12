8418b8664b9f5e56ff234c476a925ea1a6989b8c
Rob Lovelett <rlovelett@gmail.com>
2016-11-12 11:05:29 -0600

    logic outline

--- index.php
+++ index.php
@@ -28,7 +28,86 @@
     <footer></footer>
     <script type="text/javascript">
         $(document).ready(function() {
-            console.log("1");
+        //create game intro = true
+            //if game intro = true
+                //create start screen text object
+                    //header_text: Hangman
+                    //subtitle_text: Internet Cats
+                    //intro_text: Press Any Key to Get Started!
+                //display o_start_screen.header_text
+                //display o_start_screen.subtitle_text
+                //display o_start_screen.intro_text
+                    //if user presses key
+                        //set game intro = false
+                        //set game = true
+            //if game = true
+                //remove div containing o_start_screen.intro_text
+                //create array of words to use = ["bla","bla bla"]
+                //create game text object
+                    //wins_text: Wins
+                    //current_word_text: Curent Word
+                    //guesses_remaining_text: Number of Guesses Remaining
+                    //guesses_text: Letters Already Guessed
+                //output game_text.wins_text
+                //output game_text.current_word_text
+                //output game_text.guesses_text
+                //create wins = 0;
+                //set round = true;
+                    //if round = true
+                        //create hidden word string .. needs array of words to use
+                            //take array of words to use
+                            //create random word string = ""
+                                //pick a random word from array of words to use
+                                //set random word string to be random word picked from array of words to use
+                            //create random word array .. needs random word string
+                                /*split random word string to an array of strings, each entry consisting of
+                                letters/spaces from random word string*/
+                            //create hidden word array .. needs random word array
+                                /*set each entry for hidden word array to be underscores & spaces from 
+                                random word array*/
+                            //create hidden word string by combining entries of hidden word array
+                        //output hidden word string "_ _ _  _ _ _"
+                        //create guesses = 0;
+                        //create guesses array = []
+                        //create guesses string;
+                        //create win = false;
+                        //create lose = false;
+                        //if user presses key
+                            //detect what char keypress is
+                                //if keypress A-Za-z0-9
+                                    //create key = char from keypress
+                                    //compare key to entries of random word array
+                                    //if key equals one or more of the entries in random word array
+                                        //set guess_match = true
+                                        //create matches_indexes array = []
+                                        //get the index's of matching entries and store them into matches_indexes array
+                                        //for the # of matches there are
+                                            /*replace the entries inside hidden word array to the matching char from
+                                            keypress in the indexes of matches_indexes array*/
+                                    //else if key does not equal any of the entries in random word array
+                                        //set guess_match = false
+                                        //guesses++
+                                        //if guesses > 8
+                                            //set lose = true
+                                            //round = false
+                                        //store char from keypress into guesses array
+                                        //update guesses string by combining entries of guesses array
+                                        //output guesses string
+                        //for the length of random word array
+                            /*check to see if all the entries of random word array matches the entries of hidden word 
+                            array*/
+                            //if true
+                                //set win = true
+                                //wins ++
+                                //output wins
+                                //round = false
+                    //if round = false
+                        //reset
+                        //create hidden word string = ""
+                        //clear random word array = []
+                        //clear hidden word array = []
+                        //round = true
+                        
             //bank of words to use
             var a_words = ["lil bub", "brother cream", "grumpy cat", "henri", "longcat", "limecat", "nyan cat", "happy cat", "business cat", "tacgnol", "ceiling cat"];
             //instructions text
@@ -44,7 +123,7 @@
             var i_wins = 0; //game start, initialize wins to 0
             var i_guesses = 8; //game start, intialize user guesses to 8
             var s_random_word = f_choose_random(a_words);
-            console.log("2");            
+        
             //function choose random word
             function f_choose_random(word) {
                 return word[Math.floor(Math.random() * word.length)];
@@ -52,8 +131,7 @@
 
             var a_word = s_random_word.split("");
             var a_hidden_word = f_hidden_word_array(a_word);
-            console.log("3");
-
+            
             //create hidden word array "_ _ _   _ _ _"
             function f_hidden_word_array (word_array) {
                 var hidden_word_array = [];
@@ -97,7 +175,6 @@
             
             if (b_intro) {
                 document.onkeypress = function f_no_intro() {
-                    console.log("5");
                     $("#intro").remove(); //remove intro text
                     $("#hidden_word").html(o_instructions.current_word_text + "<br />" + s_hidden_word_init);
                     $("#guesses_remaining").html(o_instructions.guesses_remaining_text + "<br />" + i_guesses); //guesses text, number
@@ -106,14 +183,12 @@
                 b_intro = false; //precursor to start game
             };
 
-            console.log("8");
             var s_hidden_word = "";
             var a_guesses = [];
             var b_no_intro_keypress = true;
             
             if (b_intro !== true) {
                 document.onkeypress = function f_game() {
-                    console.log("9")
                     $("#hidden_word").html(o_instructions.current_word_text + "<br />" + s_hidden_word);
                     var key = String.fromCharCode(event.keyCode); //get keycode
                    
